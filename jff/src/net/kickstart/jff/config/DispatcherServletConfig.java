2a8bf873df5f8dfc2321e118ffe073e6a4e2afa2
AhasanHq <ahasan.hq.bd@gmail.com>
2015-03-27 16:46:16 +0600

    updated

--- jff/src/net/kickstart/jff/config/DispatcherServletConfig.java
+++
@@ -1,8 +0,0 @@
-package net.kickstart.jff.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DispatcherServletConfig {
-
-}
---
+++ jff/src/net/kickstart/jff/config/SpringConfigInitializer.java
@@ -0,0 +1,36 @@
+package net.kickstart.jff.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+//1. configuration initializer class (equivalent to web.xml)
+/*
+ * The SpringConfigInitializer class is bootstrapped automatically 
+ * by any Servlet 3.0-compliant container (e.g. Apache Tomcat 7), 
+ * and it will look for an implementation of the WebApplicationInitializer interface 
+ * and invoke its onStartup() method.
+ */
+public class SpringConfigInitializer implements WebApplicationInitializer{	
+
+	@Override
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		// registering applicationContext
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(ApplicationContextConfig.class);
+		
+		// registering and defining dispatcher servlet
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("SpringDispatcher", new DispatcherServlet(appContext));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+		
+		// trying to configure tiles - pending		
+		
+	}
+
+}
